918df3354b2ab89be544f974f1d9578cdc0cbcb4
Hannes Moser <box@hannesmoser.at>
2019-03-05 22:15:56 -0500

    fix: add missing load tests

--- test/index.js
+++ test/index.js
@@ -8,41 +8,53 @@ const fontFile = path.resolve(
 );
 
 describe('TextToSVG', () => {
-  it('get with and height', async () => {
-    const svg = await TextToSVG.load(fontFile);
-    const width = svg.getWidth('Hello, World');
-    const height = svg.getHeight(42);
+  describe('TextToSVG', () => {
+    it('load font', async () => {
+      await TextToSVG.load(fontFile);
+    });
 
-    width.should.be.a.Number().and.above(0);
-    height.should.be.a.Number().and.above(0);
+    it('load font sync', () => {
+      TextToSVG.loadSync(fontFile);
+    });
   });
 
-  it('get font metrics', async () => {
-    const svg = await TextToSVG.load(fontFile);
-    const {
-      x,
-      y,
-      baseline,
-      width,
-      height,
-      ascender,
-      descender
-    } = svg.getMetrics();
+  describe('TextToSVG', () => {
+    it('get with and height', async () => {
+      const svg = await TextToSVG.load(fontFile);
+      const width = svg.getWidth('Hello, World');
+      const height = svg.getHeight(42);
 
-    x.should.be.a.Number().and.eql(0);
-    y.should.be.a.Number().and.below(0);
-    baseline.should.be.a.Number().and.eql(0);
-    width.should.be.a.Number().and.above(0);
-    height.should.be.a.Number().and.above(0);
-    ascender.should.be.a.Number().and.above(0);
-    descender.should.be.a.Number().and.below(0);
-  });
+      width.should.be.a.Number().and.above(0);
+      height.should.be.a.Number().and.above(0);
+    });
 
-  it('get with and height', async () => {
-    const svg = await TextToSVG.load(fontFile);
-    const d = svg.getD('Hello, World');
+    it('get font metrics', async () => {
+      const svg = await TextToSVG.load(fontFile);
+      const {
+        x,
+        y,
+        baseline,
+        width,
+        height,
+        ascender,
+        descender
+      } = svg.getMetrics();
 
-    d.should.be.a.String();
-    d.length.should.be.above(0);
+      x.should.be.a.Number().and.eql(0);
+      y.should.be.a.Number().and.below(0);
+      baseline.should.be.a.Number().and.eql(0);
+      width.should.be.a.Number().and.above(0);
+      height.should.be.a.Number().and.above(0);
+      ascender.should.be.a.Number().and.above(0);
+      descender.should.be.a.Number().and.below(0);
+    });
+
+    it('get path definition', async () => {
+      const svg = await TextToSVG.load(fontFile);
+      const d = svg.getD('Hello, World');
+
+      d.should.be.a.String();
+      d.length.should.be.above(0);
+    });
   });
 });
